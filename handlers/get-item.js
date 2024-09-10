040a1c7de81d24b7040a677dca0addd323b0746d
Kazerlelutin <b@bouteiller.contact>
2024-09-10 12:15:12 +0200

    fix keys

--- handlers/get-item.js
+++ handlers/get-item.js
@@ -18,7 +18,9 @@ async function getItem(slug, req, h) {
   try {
     const companies = await getCompaniesByItemId(item.id)
     companies.forEach((c) => {
-      delete c.logo_id
+      const keys = Object.keys(c)
+      if (keys.includes('logo_id')) delete c.logo_id
+      if (keys.includes('cover_id')) delete c.cover_id
       c.medias = []
       item[c.relation_type] = c
     })
@@ -28,13 +30,19 @@ async function getItem(slug, req, h) {
       .response({ error: 'Internal server error', details: error })
       .code(500)
   }
+  const keys = Object.keys(item)
+  if (keys.includes('logo_id')) delete item.logo_id
+  if (keys.includes('cover_id')) delete item.cover_id
 
   // ====== MACHINE ========================*
   if (item.type === 'game') {
     try {
       const machine = await getMachineByGameId(item.id)
+
       if (machine) {
-        delete machine.cover_id
+        const keys = Object.keys(machine)
+        if (keys.includes('logo_id')) delete machine.logo_id
+        if (keys.includes('cover_id')) delete machine.cover_id
         item.machine = machine
         item.ref_id = machine.item_ref_id
       } else {
